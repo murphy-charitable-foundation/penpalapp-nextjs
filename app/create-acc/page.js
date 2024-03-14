7ba694e1cf011249a07a9721459a888b410d168f
RyK57 <rithviks.ryk57@gmail.com>
2024-03-14 09:44:55 -0500

    login and create fixes with Rez UI Changes

--- app/create-acc/page.js
+++ app/create-acc/page.js
@@ -1,115 +1,211 @@
-"use client"
+"use client";
 
 // pages/create-acc.js
-import { useState } from 'react';
-import { useRouter } from 'next/navigation'; 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
-import { auth, db } from '../firebaseConfig'; 
-import Link from 'next/link';
-import Image from 'next/image';
-
-
+import { auth, db } from "../firebaseConfig";
+import Link from "next/link";
+import Image from "next/image";
+import { updatePassword, signOut } from "firebase/auth";
+import { handleLogout } from "../profile/page";
 
 export default function CreateAccount() {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [birthday, setBirthday] = useState('');
-    const [password, setPassword] = useState('');
-    const [repeatPassword, setRepeatPassword] = useState('');
-    const router = useRouter(); 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const router = useRouter();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        if (password !== repeatPassword) {
-            alert("Passwords do not match.");
-            return;
-        }
-
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            const uid = userCredential.user.uid; // Get the user ID from the created user
-
-            // Create a document in Firestore in "users" collection with UID as the document key
-            await setDoc(doc(db, "users", uid), {
-                firstName,
-                lastName,
-                email, 
-                birthday, 
-            });
-
-            // Redirect to profile page or any other page as needed
-            router.push('/profile'); 
-        } catch (error) {
-            console.error("Error creating account:", error);
-            alert(error.message);
-        }
+    if (password !== repeatPassword) {
+      alert("Passwords do not match.");
+      return;
     }
 
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const uid = userCredential.user.uid; // Get the user ID from the created user
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4">
-            <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-              
-                <Link href="/">
+      // Create a document in Firestore in "users" collection with UID as the document key
+      await setDoc(doc(db, "users", uid), {
+        firstName,
+        lastName,
+        email,
+        birthday,
+      });
 
-                    <button>
-                        <svg className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
-                        </svg>
-                    </button>
-                </Link>
-                <div className="flex items-center mb-4">
-                    <svg
-                        onClick={() => window.history.back()}
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 cursor-pointer"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
-                    </svg>
-                    <h2 className="text-center text-2xl font-bold text-gray-800 ml-4">Create account</h2>
-                </div>
-                <div className="flex justify-center mb-6">
-                    <Image src="/murphylogo.png" alt="Your Logo" width={150} height={150} />
-                </div>
-                <form className="space-y-6" onSubmit={handleSubmit}>
-                    <div className="flex gap-4">
-                        <div className="w-1/2">
-                            <label htmlFor="first-name" className="text-sm font-medium text-gray-700 block mb-2">First name</label>
-                            <input id="first-name" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                        </div>
-                        <div className="w-1/2">
-                            <label htmlFor="last-name" className="text-sm font-medium text-gray-700 block mb-2">Last name</label>
-                            <input id="last-name" type="text" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="email" className="text-sm font-medium text-gray-700 block mb-2">Email</label>
-                        <input id="email" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor="birthday" className="text-sm font-medium text-gray-700 block mb-2">Birthday</label>
-                        <input id="birthday" type="date" className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" value={birthday} onChange={(e) => setBirthday(e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor="password" className="text-sm font-medium text-gray-700 block mb-2">Password</label>
-                        <input id="password" name="password" type="password" autoComplete="new-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor="repeat-password" className="text-sm font-medium text-gray-700 block mb-2">Repeat Password</label>
-                        <input id="repeat-password" name="repeatPassword" type="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} />
-                    </div>
-                    <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                        Create Account
-                    </button>
-                </form>
-            </div>
+      // Redirect to profile page or any other page as needed
+      router.push("/profile");
+    } catch (error) {
+      console.error("Error creating account:", error);
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <div className="flex items-center justify-between mb-4">
+          <svg
+            onClick={() => window.history.back()}
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 cursor-pointer"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="black"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
+          <h2 className="flex-grow text-center text-2xl font-bold text-gray-800">
+            Create account
+          </h2>
+          <div className="h-6 w-6"></div>
         </div>
+        <div className="flex justify-center mb-6">
+          <Image
+            src="/murphylogo.png"
+            alt="Your Logo"
+            width={150}
+            height={150}
+          />
+        </div>
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div className="flex gap-4">
+            <div className="w-1/2">
+              <label
+                htmlFor="first-name"
+                className="text-sm font-medium text-gray-700 block mb-2"
+              >
+                First name
+              </label>
+              <input
+                id="first-name"
+                type="text"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+            <div className="w-1/2">
+              <label
+                htmlFor="last-name"
+                className="text-sm font-medium text-gray-700 block mb-2"
+              >
+                Last name
+              </label>
+              <input
+                id="last-name"
+                type="text"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="birthday"
+              className="text-sm font-medium text-gray-700 block mb-2"
+            >
+              Birthday
+            </label>
+            <input
+              id="birthday"
+              type="date"
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              value={birthday}
+              onChange={(e) => setBirthday(e.target.value)}
+            />
+          </div>
 
-    );
+          <div>
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-gray-700 block mb-2"
+            >
+              Email
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="text-sm font-medium text-gray-700 block mb-2"
+            >
+              New Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="new-password"
+              required
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="repeat-password"
+              className="text-sm font-medium text-gray-700 block mb-2"
+            >
+              Repeat Password
+            </label>
+            <input
+              id="repeat-password"
+              name="repeatPassword"
+              type="password"
+              required
+              className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+              value={repeatPassword}
+              onChange={(e) => setRepeatPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            style={{
+              padding: "10px 20px",
+              width: "80%",
+              margin: "50px auto",
+              display: "block",
+              backgroundColor: "#48801c",
+              color: "white",
+              border: "none",
+              borderRadius: "20px",
+              cursor: "pointer",
+            }}
+          >
+            Create Account
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
