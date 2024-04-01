803426b2dd974ee9498c1dcd42214b53ef5a9228
charliefischerr <152898480+charliefischerr@users.noreply.github.com>
2024-04-01 09:23:20 -0700

    skip and continue

--- components/edit-profile.js
+++ components/edit-profile.js
@@ -1,90 +1,81 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import Cropper from 'react-cropper';
 import Dropzone from 'react-dropzone';
 import 'cropperjs/dist/cropper.css';
+import SelectProfileImage from './select-profile-image';
 
 const EditProfileImage = () => {
-  const [image, setImage] = useState(null);
-  const [croppedImage, setCroppedImage] = useState(null);
-  const [newProfileImage, setNewProfileImage] = useState(null);
-  const [previewURL, setPreviewURL] = useState(null);
-  const cropperRef = useRef();
-  const buttonClasses = () => {
-    if(!croppedImage){
-        return "w-[80%] mx-auto mt-[100px] p-2 bg-[#1C1B1F1F] text-[#1D1D00] font-semibold  rounded-[100px]"
-    } 
-    return "w-[80%] mx-auto mt-[100px] p-2 bg-[#4E802A] text-white font-semibold  rounded-[100px]"
-  }
+	const [image, setImage] = useState(null);
+	const [croppedImage, setCroppedImage] = useState(null);
+	const [newProfileImage, setNewProfileImage] = useState(null);
+	const [previewURL, setPreviewURL] = useState(null);
+	const [stage, setStage] = useState(0);
+	const cropperRef = useRef();
+	const buttonClasses = () => {
+		if (!croppedImage) {
+			return "w-[80%] mx-auto mt-[100px] p-2 bg-[#1C1B1F1F] text-[#1D1D00] font-semibold  rounded-[100px]"
+		}
+		return "w-[80%] mx-auto mt-[100px] p-2 bg-[#4E802A] text-white font-semibold  rounded-[100px]"
+	}
 
-  const handleDrop = (acceptedFiles) => {
-    setImage(URL.createObjectURL(acceptedFiles[0]));
-  };
+	const handleDrop = (acceptedFiles) => {
+		setImage(URL.createObjectURL(acceptedFiles[0]));
+	};
 
-  const handleCrop = () => {
-    if (cropperRef.current && typeof cropperRef.current?.cropper?.getCroppedCanvas === 'function') {
-        const canvas = cropperRef.current.cropper.getCroppedCanvas();
-        canvas.toBlob((blob) => {
-            setCroppedImage(blob);
-        });
-    }
-  };
+	const handleCrop = () => {
+		if (cropperRef.current && typeof cropperRef.current?.cropper?.getCroppedCanvas === 'function') {
+			const canvas = cropperRef.current.cropper.getCroppedCanvas();
+			canvas.toBlob((blob) => {
+				setCroppedImage(blob);
+			});
+		}
+	};
 
-  const onSave = () => {
-    setNewProfileImage(croppedImage)
-    setPreviewURL(URL.createObjectURL(croppedImage));
-  }
+	const onSave = () => {
+		setNewProfileImage(croppedImage)
+		setPreviewURL(URL.createObjectURL(croppedImage));
+	}
 
-  
+	const handleSave = () => {
+		onSave();
+		// Reset state
+		setImage(null);
+	};
 
-  const handleSave = () => {
-    // Here you can save the cropped image to Firestore or do any other processing
-    onSave();
-    // Reset state
-    setImage(null);
-    // setCroppedImage(null);
-  };
+	const resetAll = () => {
+		setImage(null)
+		setNewProfileImage(null)
+		setCroppedImage(null)
+		setPreviewURL(null)
+	}
 
-  return (
-    <div className='flex flex-col'>
-      <h2 className='text-[#034792] text-center text-2xl font-bold my-[64px]'>Add a profile avatar</h2>
-      {!image ? (
-      <Dropzone onDrop={handleDrop}>
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p className='bg-[#65B427] rounded-[50%] h-[220px] w-[220px] mx-auto cursor-pointer flex flex-center'>
-                {newProfileImage ? (
-                    <img src={previewURL} className='m-auto my-auto rounded-[50%]' />
-                ) : (
-                    <img src='/cameraicon.png' className='m-auto my-auto' />
-                )}
-            </p>
-            </div>
-          </section>
-        )}
-      </Dropzone>
+	const updateStage = (skip=false) => {
+		if(skip){
+			resetAll()
+		}
+		setStage(1)
+	}
 
-      ) : (
-            <div>
-              <h3>Preview</h3>
-              <Cropper
-                src={image}
-                style={{ height: 400, width: '100%'}}
-                aspectRatio={1}
-                guides={true}
-                crop={handleCrop}
-                ref={cropperRef}
-                viewMode={1}
-              />
-              <br />
-              <button onClick={handleSave}>Save</button>
-            </div>
-      )}
-      <button className={buttonClasses()}>Continue</button>
-      <button className='text-black font-semibold mt-[14px] mb-[40px]'>Skip for now</button>
-    </div>
-  );
+	return (
+		<div className='flex flex-col'>
+			{stage === 0 && (
+				<SelectProfileImage 
+					image={image}
+					handleSave={handleSave}
+					newProfileImage={newProfileImage}
+					previewURL={previewURL}
+					buttonClasses={buttonClasses}
+					handleDrop={handleDrop}
+					handleCrop={handleCrop}
+					cropperRef={cropperRef}
+					updateStage={updateStage}
+				/>
+			)}
+			{stage === 1 && (
+				<></>
+			)}
+		</div>
+	);
 };
 
 export default EditProfileImage;
---
+++ components/select-profile-image.js
@@ -0,0 +1,50 @@
+import { useState, useRef } from 'react';
+import Cropper from 'react-cropper';
+import Dropzone from 'react-dropzone';
+import 'cropperjs/dist/cropper.css';
+
+const SelectProfileImage = ({image, handleSave, newProfileImage, previewURL, buttonClasses, handleDrop, handleCrop, cropperRef, updateStage}) => {
+	return (
+		<div className='flex flex-col'>
+			<h2 className='text-[#034792] text-center text-2xl font-bold my-[64px]'>Add a profile avatar</h2>
+			{/* {stage === 0 ? ()} */}
+			{!image ? (
+				<Dropzone onDrop={handleDrop}>
+					{({ getRootProps, getInputProps }) => (
+						<section>
+							<div {...getRootProps()}>
+								<input {...getInputProps()} />
+								<p className='bg-[#65B427] rounded-[50%] h-[220px] w-[220px] mx-auto cursor-pointer flex flex-center'>
+									{newProfileImage ? (
+										<img src={previewURL} className='m-auto my-auto rounded-[50%]' />
+									) : (
+										<img src='/cameraicon.png' className='m-auto my-auto' />
+									)}
+								</p>
+							</div>
+						</section>
+					)}
+				</Dropzone>
+			) : (
+				<div>
+					<h3>Preview</h3>
+					<Cropper
+						src={image}
+						style={{ height: 400, width: '100%' }}
+						aspectRatio={1}
+						guides={true}
+						crop={handleCrop}
+						ref={cropperRef}
+						viewMode={1}
+					/>
+					<br />
+					<button onClick={handleSave}>Save</button>
+				</div>
+			)}
+			<button className={buttonClasses()} onClick={() => updateStage(false)}>Continue</button>
+			<button className='text-black font-semibold mt-[14px] mb-[40px]' onClick={() => updateStage(true)}>Skip for now</button>
+		</div>
+	);
+};
+
+export default SelectProfileImage;
