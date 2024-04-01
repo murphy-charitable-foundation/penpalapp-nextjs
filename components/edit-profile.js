5660ab94fcdeae205c583551705227fc94513d04
charliefischerr <152898480+charliefischerr@users.noreply.github.com>
2024-04-01 09:11:51 -0700

    can add profile pic and crops to a circle, needs configuring to firestore

--- components/edit-profile.js
+++ components/edit-profile.js
@@ -11,9 +11,9 @@ const EditProfileImage = () => {
   const cropperRef = useRef();
   const buttonClasses = () => {
     if(!croppedImage){
-        return "w-[80%] mx-auto mt-[100px] p-2 bg-[#1C1B1F1F] text-[#1D1D00] rounded-[100px]"
+        return "w-[80%] mx-auto mt-[100px] p-2 bg-[#1C1B1F1F] text-[#1D1D00] font-semibold  rounded-[100px]"
     } 
-    return "w-[80%] mx-auto mt-[100px] p-2 bg-[#4E802A] text-white rounded-[100px]"
+    return "w-[80%] mx-auto mt-[100px] p-2 bg-[#4E802A] text-white font-semibold  rounded-[100px]"
   }
 
   const handleDrop = (acceptedFiles) => {
@@ -47,6 +47,7 @@ const EditProfileImage = () => {
   return (
     <div className='flex flex-col'>
       <h2 className='text-[#034792] text-center text-2xl font-bold my-[64px]'>Add a profile avatar</h2>
+      {!image ? (
       <Dropzone onDrop={handleDrop}>
         {({ getRootProps, getInputProps }) => (
           <section>
@@ -54,7 +55,7 @@ const EditProfileImage = () => {
               <input {...getInputProps()} />
               <p className='bg-[#65B427] rounded-[50%] h-[220px] w-[220px] mx-auto cursor-pointer flex flex-center'>
                 {newProfileImage ? (
-                    <img src={previewURL} className='m-auto my-auto' />
+                    <img src={previewURL} className='m-auto my-auto rounded-[50%]' />
                 ) : (
                     <img src='/cameraicon.png' className='m-auto my-auto' />
                 )}
@@ -63,24 +64,25 @@ const EditProfileImage = () => {
           </section>
         )}
       </Dropzone>
-      {image && (
-        <div>
-          <h3>Preview</h3>
-          <Cropper
-            src={image}
-            style={{ height: 400, width: '100%' }}
-            aspectRatio={1}
-            guides={true}
-            crop={handleCrop}
-            ref={cropperRef}
-            viewMode={1}
-          />
-          <br />
-          <button onClick={handleSave}>Save</button>
-        </div>
-      )}
 
+      ) : (
+            <div>
+              <h3>Preview</h3>
+              <Cropper
+                src={image}
+                style={{ height: 400, width: '100%'}}
+                aspectRatio={1}
+                guides={true}
+                crop={handleCrop}
+                ref={cropperRef}
+                viewMode={1}
+              />
+              <br />
+              <button onClick={handleSave}>Save</button>
+            </div>
+      )}
       <button className={buttonClasses()}>Continue</button>
+      <button className='text-black font-semibold mt-[14px] mb-[40px]'>Skip for now</button>
     </div>
   );
 };
