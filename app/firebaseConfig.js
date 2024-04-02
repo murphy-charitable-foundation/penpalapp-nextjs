17cf109ed9d9e7f19c069a63b2bced7577b4c3ff
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-04-02 00:19:52 -0400

    enabled analytics instance(may need to enable online

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -3,7 +3,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -27,7 +27,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const analytics = getAnalytics(app);
 
-export { db, auth };
+export { db, auth, analytics };
 
 // Initialize Firebase Authentication and export
