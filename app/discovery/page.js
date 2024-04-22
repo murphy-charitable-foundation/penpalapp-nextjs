cef4c5486552fae3f0b1e3b540d23bf61b6ae27c
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-04-22 18:12:32 -0400

    kid card updated

--- app/discovery/page.js
+++ app/discovery/page.js
@@ -8,6 +8,7 @@ import { db } from '../firebaseConfig'; // Ensure this path is correct
 import { collection, getDocs } from 'firebase/firestore';
 import { differenceInCalendarYears } from 'date-fns';
 import BottomNavBar from '@/components/bottom-nav-bar';
+import KidCard from '@/components/general/KidCard';
 
 
 export default function ChooseKid() {
@@ -89,34 +90,35 @@ export default function ChooseKid() {
 
                     {/* Card components */}
                     {kids.map((kid) => (
-                        <div key={kid.id} className="w-full max-w-sm my-4 p-4 rounded-lg shadow-lg flex flex-col items-start"> {/* Removed center alignment */}
-                            <div className="w-32 h-32 overflow-hidden rounded-full mx-auto"> {/* Profile image container */}
-                                <Image
-                                    src={kid.image || '/usericon.png'}
-                                    alt={kid.firstName}
-                                    width={128}
-                                    height={128}
-                                    className="object-cover"
-                                />
-                            </div>
-                            <h2 className="text-xl mt-3 mb-1 text-left" style={{ color: '#262626' }}>{kid.firstName}</h2> {/* Text aligned left */}
-                            <p className="text-xs mb-1 text-left text-black">{calculateAge(kid.birthday)} years old</p> {/* Text aligned left */}
-                            <p className="text-left mb-2 text-gray-900 text-xs" style={{ color: '#515151' }}>{kid.bio}</p> {/* Text aligned left */}
-                            <div className="flex justify-start flex-wrap gap-2 mb-4"> {/* Tags container */}
-                                {kid.interests?.map((interest, idx) => (
-                                    <span key={idx} className="px-3 py-1 text-xs rounded-full" style={{ backgroundColor: '#fea500', color: 'white' }}>
-                                        {interest}
-                                    </span>
-                                ))}
-                            </div>
-                            <div className="self-end mt-auto"> {/* Button aligned to the right */}
-                                <Link href="/letterwrite">
-                                    <button className="w-28 py-2 rounded-3xl text-center text-xs" style={{ backgroundColor: '#0369a1', color: 'white' }}>
-                                        Send a message
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>
+                        // <div key={kid.id} className="w-full max-w-sm my-4 p-4 rounded-lg shadow-lg flex flex-col items-start"> {/* Removed center alignment */}
+                        //     <div className="w-32 h-32 overflow-hidden rounded-full mx-auto"> {/* Profile image container */}
+                        //         <Image
+                        //             src={kid.image || '/usericon.png'}
+                        //             alt={kid.firstName}
+                        //             width={128}
+                        //             height={128}
+                        //             className="object-cover"
+                        //         />
+                        //     </div>
+                        //     <h2 className="text-xl mt-3 mb-1 text-left" style={{ color: '#262626' }}>{kid.firstName}</h2> {/* Text aligned left */}
+                        //     <p className="text-xs mb-1 text-left text-black">{calculateAge(kid.birthday)} years old</p> {/* Text aligned left */}
+                        //     <p className="text-left mb-2 text-gray-900 text-xs" style={{ color: '#515151' }}>{kid.bio}</p> {/* Text aligned left */}
+                        //     <div className="flex justify-start flex-wrap gap-2 mb-4"> {/* Tags container */}
+                        //         {kid.interests?.map((interest, idx) => (
+                        //             <span key={idx} className="px-3 py-1 text-xs rounded-full" style={{ backgroundColor: '#fea500', color: 'white' }}>
+                        //                 {interest}
+                        //             </span>
+                        //         ))}
+                        //     </div>
+                        //     <div className="self-end mt-auto"> {/* Button aligned to the right */}
+                        //         <Link href="/letterwrite">
+                        //             <button className="w-28 py-2 rounded-3xl text-center text-xs" style={{ backgroundColor: '#0369a1', color: 'white' }}>
+                        //                 Send a message
+                        //             </button>
+                        //         </Link>
+                        //     </div>
+                        // </div>
+                        <KidCard kid={kid} calculateAge={calculateAge} key={kid?.id} />
                     ))}
 
 
--- components/general/KidCard.jsx
+++ components/general/KidCard.jsx
@@ -1,69 +1,58 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 //Card for kid
-export default function KidCard({ kid }) {
+export default function KidCard({ kid, calculateAge }) {
   return (
-    <div>
-      <div
-        key={kid.id}
-        className="w-full max-w-sm my-4 p-4 rounded-lg shadow-lg flex flex-col items-start"
+    <div
+      key={kid.id}
+      className="w-full max-w-sm my-4 p-4 rounded-lg shadow-lg flex flex-col items-start"
+    >
+<div className="w-48 h-48 overflow-hidden rounded-full mx-auto"> {/* Adjusted width and height */}
+  <Image
+    src={kid.image || "/usericon.png"}
+    alt={kid.firstName}
+    width={220} 
+    height={220} 
+    className="object-cover"
+  />
+</div>
+
+      <h2 className="text-xl mt-3 mb-1 text-left text-bold" style={{ color: "#262626" }}>
+        {kid.firstName}
+      </h2>
+
+      <p className="text-xs mb-1 text-left text-black">
+        {calculateAge(kid.birthday)} years old
+      </p>
+      <p
+        className="text-left mb-2 text-gray-900 text-xs break-words"
+        style={{ color: "#515151" }}
       >
-        {" "}
-        {/* Removed center alignment */}
-        <div className="w-32 h-32 overflow-hidden rounded-full mx-auto">
-          {" "}
-          {/* Profile image container */}
-          <Image
-            src={kid.image || "/usericon.png"}
-            alt={kid.firstName}
-            width={128}
-            height={128}
-            className="object-cover"
-          />
-        </div>
-        <h2
-          className="text-xl mt-3 mb-1 text-left"
-          style={{ color: "#262626" }}
-        >
-          {kid.firstName}
-        </h2>{" "}
-        {/* Text aligned left */}
-        <p className="text-xs mb-1 text-left text-black">
-          {calculateAge(kid.birthday)} years old
-        </p>{" "}
-        {/* Text aligned left */}
-        <p
-          className="text-left mb-2 text-gray-900 text-xs"
-          style={{ color: "#515151" }}
-        >
-          {kid.bio}
-        </p>{" "}
-        {/* Text aligned left */}
-        <div className="flex justify-start flex-wrap gap-2 mb-4">
-          {" "}
-          {/* Tags container */}
-          {kid.interests?.map((interest, idx) => (
-            <span
-              key={idx}
-              className="px-3 py-1 text-xs rounded-full"
-              style={{ backgroundColor: "#fea500", color: "white" }}
-            >
-              {interest}
-            </span>
-          ))}
-        </div>
-        <div className="self-end mt-auto">
-          {" "}
-          {/* Button aligned to the right */}
-          <Link href="/letterwrite">
-            <button
-              className="w-28 py-2 rounded-3xl text-center text-xs"
-              style={{ backgroundColor: "#0369a1", color: "white" }}
-            >
-              Send a message
-            </button>
-          </Link>
-        </div>
+        {kid.bio}
+      </p>
+      <div className="flex justify-start flex-wrap gap-2 mb-4">
+        {/* {kid.interests?.map((interest, idx) => (
+          <span
+            key={idx}
+            className="px-3 py-1 text-xs rounded-full"
+            style={{ backgroundColor: "#fea500", color: "white" }}
+          >
+            {interest}
+          </span>
+        ))} */}
+        <p className="text-black break-words text-xs">{kid?.hobby}</p>
+      </div>
+      <div className="self-end mt-auto">
+        <Link href="/letterwrite">
+          <button
+            className="w-28 py-2 rounded-3xl text-center text-xs"
+            style={{ backgroundColor: "#0369a1", color: "white" }}
+          >
+            Send a message
+          </button>
+        </Link>
       </div>
     </div>
   );
