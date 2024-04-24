41257f15a9412d4c49afbc646f2331210c142deb
charliefischerr <152898480+charliefischerr@users.noreply.github.com>
2024-04-23 19:18:02 -0700

    reordering of items on messages screen

--- app/letterwrite/page.js
+++ app/letterwrite/page.js
@@ -115,7 +115,7 @@ export default function WriteLetter() {
 
 
   const selectUser = (user) => {
-    setSelectedUser({...user, profile_picture: "https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg"});
+    setSelectedUser({ ...user, profile_picture: "https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg" });
     closeRecipientModal(); // Close the modal upon selection
   };
 
@@ -139,15 +139,30 @@ export default function WriteLetter() {
         <div className="flex items-center justify-between p-4 border-b border-gray-300 bg-[#FAFAFA]">
           <Link href="/">
             <button onClick={() => window.history.back()}>
-              <svg className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <img src="/closeicon.svg" />
+              {/* <svg className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
-              </svg>
+              </svg> */}
             </button>
           </Link>
-          <h1 className="text-xl font-bold text-center flex-grow text-black">
-            Write a letter
-          </h1>
           <button className="opacity-0">{"<"}</button>
+          {/* Attachments and Actions */}
+          <div className="flex justify-between items-center p-4">
+            <span className="text-black">0 files</span>
+            <div className="space-x-2">
+              <button className="text-black p-2 rounded-full">
+                <FiFileText className="h-6 w-6" />
+              </button>
+
+              <button
+                className="text-black p-2 rounded-full"
+                onClick={handleSendLetter}
+              >
+                <FiSend className="h-6 w-6" />
+              </button>
+            </div>
+          </div>
+
         </div>
 
         {/* Recipient Info */}
@@ -217,25 +232,6 @@ export default function WriteLetter() {
           value={letterContent}
           onChange={(e) => setLetterContent(e.target.value)}
         />
-
-        {/* Attachments and Actions */}
-        <div className="flex justify-between items-center p-4 bg-[#F3F4F6] border-t border-gray-300">
-          <span className="text-black">0 files</span>
-          <div className="space-x-2">
-            <button className="text-black p-2 rounded-full">
-              <FiFileText className="h-6 w-6" />
-            </button>
-            <button className="text-black p-2 rounded-full">
-              <FiMic className="h-6 w-6" />
-            </button>
-            <button
-              className="text-black p-2 rounded-full"
-              onClick={handleSendLetter}
-            >
-              <FiSend className="h-6 w-6" />
-            </button>
-          </div>
-        </div>
 
         {/* Character Count */}
         <div className="text-right text-sm p-4 text-gray-600">
