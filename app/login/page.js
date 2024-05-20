7acde26b78d14923e092ab43ad180cb550d8a730
GU <90733954+jg6604@users.noreply.github.com>
2024-05-19 22:11:31 -0400

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -35,8 +35,7 @@ export default function Login() {
             console.error("Authentication error:", error.message);
             switch (error.code) {
                 case 'auth/user-not-found':
-                    setError('The email is not correct.');
-                    
+                    setError('The email is not correct.');                    
                     break;
                 case 'auth/wrong-password':
                     setError('The password is not correct.');
@@ -59,113 +58,97 @@ export default function Login() {
     //};
 
     return (
+            
+            //<div className="w-full max-w-md space-y-8 bg-white p-8 min-h-screen">
+        <div className="bg-white p-8 min-h-screen">
+        <div style={{ textAlign: 'center', padding: '0px', background: 'white' }}>
+            <div className="flex flex-row items-center justify-between mb-4">
+                <svg
+                    onClick={() => window.history.back()}
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 cursor-pointer"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="black"
+                >
+                </svg>
+                <h2 className="flex-grow text-center text-2xl font-bold text-gray-800">Login</h2>
+                <div className="h-6 w-6"></div>
+            </div>
+      <div className="flex justify-center">
+        <Image src={logo} alt="Foundation Logo" width={200} margin={0}/>
+      </div>
 
-        <div>
-            <div className="w-full max-w-md space-y-8 bg-white p-8 min-h-screen">
-            <div style={{ textAlign: 'left', padding: '0px', background: 'white' }}>
-                <div className="flex flex-row items-center justify-center ">
-                <Link href="/">
-                    <button style={{position:'absolute', left:30, border:'none', background:'none'}}>
-                        <svg className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
-                        </svg>
-                    </button>
-                </Link>
-                <h2 className="mt-6 text-center text-2xl font-bold text-gray-800">
-                    Login
-                </h2>
-                </div>
-
-                <div className="flex justify-center mb-6 mt-6">
-
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-6">
-            <div className="w-full max-w-md space-y-8">
-            <div style={{ textAlign: 'left', padding: '20px', background: 'white' }}>
-            <button onClick={() => window.history.back()}>
-                    <svg className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
-                    </svg>
-                </button>
-                <div className="flex justify-center">
-
-                    <Image
-                        src={logo}
-                        alt="Murphy Charitable Foundation Uganda"
-                        width={150} 
-                        height={150} 
-                    />
-                </div>
-
-                <form className="mt-14 space-y-12 " onSubmit={handleSubmit}>
-                        <div>
-                        <h6 className="mt-10 text-left ml-2 text-gray-800 ">Email</h6>
-                        <input
-                            type="email"
-                            value={email}
-                            autoComplete="email"
-                            required
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Ex. user@gmail.com"
-                            style={{ border: '0px', borderBottom: '1px solid black', padding: '10px', width: '100%', margin: '0 auto', display:'block', color: 'black' }}
-                            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+      <form className="mt-14 space-y-12 " onSubmit={handleSubmit}>
+            <div>
+                <h6 className="mt-10 text-left ml-2 text-gray-800 ">Email</h6>
+                    <input
+                        type="email"
+                        value={email}
+                        autoComplete="email"
+                        required
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Ex. user@gmail.com"
+                        style={{ border: '0px', borderBottom: '1px solid black', padding: '10px', width: '100%', margin: '0 auto', display:'block', color: 'black' }}
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
                              
-                        />
-                        </div>
+                    />
+            </div>
                         
                         
-                        <div>
-                        <h6 className="mt-12 text-left ml-2 text-gray-800 ">Password</h6>
-                        <input
-                            type="password"
-                            value={password}
-                            autoComplete="current-password"
-                            required
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="******"
-                            style={{ border: '0px', borderBottom: '1px solid black', padding: '10px', width: '100%', margin: '0 auto', display: 'block', color: 'black' }}
-                            className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" 
+            <div>
+                <h6 className="mt-12 text-left ml-2 text-gray-800 ">Password</h6>
+                    <input
+                        type="password"
+                        value={password}
+                        autoComplete="current-password"
+                        required
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="******"
+                        style={{ border: '0px', borderBottom: '1px solid black', padding: '10px', width: '100%', margin: '0 auto', display: 'block', color: 'black' }}
+                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" 
         
-                        />
-                        </div>
+                    />
+            </div>
 
-                        {error && <span className="flex items-left text-sm text-red-500">{error}</span>}
+                {error && <span className="flex items-left text-sm text-red-500">{error}</span>}
                         
-                    <div className="text-sm">
-                            <a href="/reset-password" style={{textDecoration:'underline'}} className="font-medium text-gray-600 hover:text-blue-500">
-                                Forgot your password?
-                            </a>
-                    </div>
+            <div className="text-left text-sm">
+                    <a href="/reset-password" style={{textDecoration:'underline'}} className="font-medium text-gray-600 hover:text-blue-500">
+                    Forgot your password?
+                    </a>
+            </div>
 
-                    <div className="flex items-center justify-between">
-                        <div className="flex items-center">
-                            <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
-                            <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                                Remember me
-                            </label>
-                        </div>
-                    </div>
+            <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                    <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
+                    <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                        Remember me
+                    </label>
+                </div>
+            </div>
 
-                    <div >
+            <div >
                         
-                        <button 
-                           type="submit"                            
-                           style={{
-                           padding:'10px 20px',
-                           width:'80%', 
-                           margin:'50px auto', 
-                           display:'block',
-                           borderRadius:'20px',
-                           cursor:'pointer',
-                           marginTop:"120px",
-                           hover:'enabled',
-                           }} 
-                           className="group relative  w-full flex justify-center py-2 px-4 border border-transparent rounded-full text-sm font-medium  text-gray-400 bg-gray-200 hover:bg-[#48801c] hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-transparent"
-                           disabled={email===""}                          
-                        > 
-                        Login
-                        </button>                                       
-                    </div>
-                </form>
+                <button 
+                    type="submit"                            
+                    style={{
+                    padding:'10px 20px',
+                    width:'80%', 
+                    margin:'50px auto', 
+                    display:'block',
+                    borderRadius:'20px',
+                    cursor:'pointer',
+                    marginTop:"120px",
+                    hover:'enabled',
+                    }} 
+                    className="group relative  w-full flex justify-center py-2 px-4 border border-transparent rounded-full text-sm font-medium  text-gray-400 bg-gray-200 hover:bg-[#48801c] hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-transparent"
+                    disabled={email===""}                          
+                > 
+                    Login
+                </button>                                       
+            </div>
+        </form>
 
         {showModal && (
         <div style={{
@@ -203,6 +186,6 @@ export default function Login() {
 
      </div>
     </div>
-    </div>
+    
     );
 }
