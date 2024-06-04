9d54f0db26b7b66fab40abc5abf359239d92b1fd
GU <90733954+jg6604@users.noreply.github.com>
2024-06-03 22:02:15 -0400

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -120,15 +120,6 @@ export default function Login() {
                     </a>
             </div>
 
-            <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                    <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
-                    <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                        Remember me
-                    </label>
-                </div>
-            </div>
-
             <div >
                         
                 <button 
