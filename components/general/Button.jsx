4c6b62196ba4b049b70a71562e84a1881e87ab52
jibinjoy8897 <jibinjoy8897@gmail.com>
2024-09-09 19:28:04 -0700

    Log In Page UI Fix with Remember Me functionality completed

--- components/general/Button.jsx
+++ components/general/Button.jsx
@@ -1,8 +1,18 @@
-export default function Button({ color, btnText, hoverColor, btnType }) {
+export default function Button({
+  color,
+  textColor,
+  btnText,
+  hoverColor,
+  btnType,
+  disabled,
+  hoverTextClr,
+  font,
+}) {
   return (
     <button
       type={btnType === "submit" ? "submit" : "button"}
-      className={`${color} ${hoverColor} text-white font-bold py-2 px-4  rounded-md  w-72`}
+      disabled={disabled}
+      className={`text-sm font-medium  ${color} ${hoverColor} ${textColor} ${hoverTextClr} ${font}  font-bold py-2 px-4  rounded-md  w-72`}
     >
       {btnText}
     </button>
