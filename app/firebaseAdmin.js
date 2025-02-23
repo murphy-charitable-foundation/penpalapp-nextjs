680f0a57bac02fbf5f7c3ac7bb96577421d52ceb
mustankap <mustankap@gmail.com>
2025-02-23 00:31:18 -0500

    Fix: username and name not visible on refresh letterhome

--- app/firebaseAdmin.js
+++ app/firebaseAdmin.js
@@ -1,16 +1,16 @@
-import * as admin from 'firebase-admin';
-
+import * as admin from "firebase-admin";
+import serviceAccountPath from "./firebase-service-account.json";
 //import serviceAccount from '../penpalmagicapp-firebase-adminsdk-czr2x-7f1623a3ac.json';
 
-const serviceAccount = {
-  projectId: "penpalmagicapp",
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Handle line breaks in private key
-};
+// const serviceAccount = {
+//   projectId: "penpalmagicapp",
+//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // Handle line breaks in private key
+// };
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccountPath),
   });
 }
 
