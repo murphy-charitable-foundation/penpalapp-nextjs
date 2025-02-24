d32b3a4921239aa7b8a2f74757abcff63d5905e9
Connor <connorlwhite@icloud.com>
2025-02-24 08:11:14 -0500

    Removed redundant if statement

--- app/firebaseAdmin.js
+++ app/firebaseAdmin.js
@@ -2,15 +2,9 @@ import * as admin from "firebase-admin";
 import * as Sentry from "@sentry/node"; 
 
 // Check if Firebase credentials exist
-var hasValidCredentials;
+
+
 if (process.env.FIREBASE_CLIENT_EMAIL && process.env.FIREBASE_PRIVATE_KEY) {
-  hasValidCredentials = true;
-} else {
-  hasValidCredentials = false;
-}
-
-
-if (hasValidCredentials == true) {
   const serviceAccount = {
     projectId: "penpalmagicapp",
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
@@ -22,7 +16,6 @@ if (hasValidCredentials == true) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
     });
-    
   }
 } else {
   const errorMessage = "Firebase Admin SDK is not initialized due to missing credentials.";
