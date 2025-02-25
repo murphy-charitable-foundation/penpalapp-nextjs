941a4b4d175ca3d17a0fb20cd400b17406aa0fbd
mustankap <mustankap@gmail.com>
2025-02-25 11:37:12 -0500

    firebase admin

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,4 +1,4 @@
-// // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { getStorage } from "@firebase/storage";
 import { initializeApp } from "@firebase/app";
 import { getAuth } from "firebase/auth";
@@ -6,45 +6,6 @@ import { getFirestore, FieldPath } from "firebase/firestore";
 
 // import { getAnalytics } from "firebase/analytics";
 // todo Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBpYg-KAzwWGaT3g7J8smjnNqP8N8Nj8vQ",
-//   authDomain: "penpalmagicapp.firebaseapp.com",
-//   projectId: "penpalmagicapp",
-//   storageBucket: "penpalmagicapp.appspot.com",
-//   messagingSenderId: "45289060638",
-//   appId: "1:45289060638:web:33121bc47d40ceef83f10f",
-//   measurementId: "G-FG3MPZ8JV6",
-// };
-
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// // const storage = getStorage(app, "gs://penpalmagicapp.appspot.com/");
-// const storage = getStorage(app);
-
-// Initialize Firebase Authentication and export
-
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -59,11 +20,16 @@ const firebaseConfig = {
   measurementId: "G-FG3MPZ8JV6",
 };
 
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
-const storage = getStorage(app);
+const storage = getStorage(app, "gs://penpalmagicapp.appspot.com/");
 
 export { db, auth, storage, FieldPath };
+
+// Initialize Firebase Authentication and export
