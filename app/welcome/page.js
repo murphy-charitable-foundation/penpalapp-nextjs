7c9c35007800b3cabb9cef0892490be72237ec6b
bubuy <dave@intang.cn>
2025-04-09 16:39:47 -0700

    update:adjusted the welcome page for better responsiveness

--- app/welcome/page.js
+++ app/welcome/page.js
@@ -18,21 +18,16 @@ export default function Welcome() {
     }, []);
     return (
         <div className="min-h-screen !bg-[#034792]">
-            <div className="max-w-lg mx-auto text-white">
-                <Image
-                    src="/welcome.png"
-                    width={393}
-                    height={411}
-                    layout="responsive"
-                    alt='Welcome'
-                />
+            <div className="max-w-lg mx-auto text-white flex flex-col min-h-screen">
+                <div className="relative w-full h-[50vh] bg-[url('/welcome.png')] bg-cover bg-center">
+                </div>
                 <h3 className='pt-16 text-center w-full font-[700] text-2xl'>
                     Welcome, {firstName}
                 </h3>
-                <div className='text-center w-full pt-5'>
+                <div className='text-center w-full pt-5 flex-1'>
                 We are so happy to be here, thanks for your support. Now you are part of the family. 
                 </div>
-                <div className='text-center w-full fixed bottom-10 left-0'>
+                <div className='text-center w-full pt-10 pb-20'>
                     <Link href="/edit-profile-user-image">
                         <button className="bg-white text-[#111111] px-16 py-2 rounded-full font-semibold">
                         Continue
