e1d6dd65a573ec783c2aed921c7810260b106b4b
Suyeong <81769862+su794@users.noreply.github.com>
2025-04-11 09:54:20 -0700

    Loading Spinner

---
+++ components/loading/LoadingSpinner.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function LoadingSpinner({isPending}) {
+    const imageStyle = {
+        borderRadius: '100px',
+        border: '1px solid #fff',
+    }
+  return (
+    
+    <>
+        {
+            isPending &&
+            <div className='fixed top-0 right-0 left-0 bottom-0 bg-black bg-opacity-95 z-20'>
+            {/* <div className='fixed top-0 right-0 left-0 bottom-0 bg-white bg-opacity-95 z-20'> */}
+                <div className='centered-thing z-50'>
+                    <div className="w-[200px] h-[200px] spinner flex flex-wrap items-center justify-center">
+                        <div className="spinner-circle w-[200px] h-[200px] border-8 border-solid border-blue-100 border-t-red-300 rounded-full animate-spin"></div>
+                        <div className='w-full text-center'>
+                            <p className='text-white text-3xl'>Loading...</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        }
+    </>
+    
+  )
+}
