93192c1f45c9462c3841d4261ecf1fe653d1d545
Suyeong <81769862+su794@users.noreply.github.com>
2025-04-19 11:51:35 -0700

    Color tweaks on the loading spinner

--- components/loading/LoadingSpinner.jsx
+++ components/loading/LoadingSpinner.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import styles from './LoadingSpinner.module.css';
 
 export default function LoadingSpinner({isPending}) {
-    const imageStyle = {
-        borderRadius: '100px',
-        border: '1px solid #fff',
-    }
   return (
     
     <>
         {
             isPending &&
-            <div className='fixed top-0 right-0 left-0 bottom-0 bg-black bg-opacity-95 z-20'>
+            <div className='fixed top-0 right-0 left-0 bottom-0 bg-white z-20'>
             {/* <div className='fixed top-0 right-0 left-0 bottom-0 bg-white bg-opacity-95 z-20'> */}
                 <div className='centered-thing z-50'>
                     <div className="w-[200px] h-[200px] spinner flex flex-wrap items-center justify-center">
-                        <div className="spinner-circle w-[200px] h-[200px] border-8 border-solid border-blue-100 border-t-red-300 rounded-full animate-spin"></div>
+                        {/* Spinner */}
+                        <div className={styles['loading-wrap']}>
+                            <div className={styles['loading-inner']}></div>
+                        </div>
                         <div className='w-full text-center'>
-                            <p className='text-white text-3xl'>Loading...</p>
+                            <p className='text-xl'>Loading...</p>
                         </div>
                     </div>
                 </div>
