a0b945cf95173b2a0809acb4c5033e06cfb99d21
studentuser13 <fraserr01@gmail.com>
2025-05-17 20:12:31 -0400

    Fixed: Missing "key" prop for element in iterator 2
    Removed first letter of first name key from the child element of the encapsulating Link element with the new id key

--- components/general/letter/RecipientList.jsx
+++ components/general/letter/RecipientList.jsx
@@ -1,29 +1,30 @@
 import Link from "next/link";
 import ProfileImage from "../ProfileImage";
 
-
 export default function RecipientList({ recipients }) {
   if (!recipients?.length) return null;
 
   return (
     <div className="flex space-x-6 p-4 bg-[#F3F4F6] rounded-t-lg">
-      {recipients.map(recipient => (
+      {recipients.map((recipient) => (
         <Link key={recipient?.id} href={"/profile-view/" + recipient?.id}>
-        <div key={recipient?.first_name?.[0]} className="flex items-center space-x-3">
-          <ProfileImage 
-            photo_uri={recipient?.photo_uri} 
-            first_name={recipient?.first_name} 
-            size={10}
-          />
-          <div>
-            <h2 className="font-bold text-sm text-black">
-              {recipient?.first_name} {recipient?.last_name}
-            </h2>
-            <p className="text-xs text-gray-500">{recipient?.country}</p>
+          <div
+            className="flex items-center space-x-3"
+          >
+            <ProfileImage
+              photo_uri={recipient?.photo_uri}
+              first_name={recipient?.first_name}
+              size={10}
+            />
+            <div>
+              <h2 className="font-bold text-sm text-black">
+                {recipient?.first_name} {recipient?.last_name}
+              </h2>
+              <p className="text-xs text-gray-500">{recipient?.country}</p>
+            </div>
           </div>
-        </div>
         </Link>
       ))}
     </div>
   );
-} 
+}
