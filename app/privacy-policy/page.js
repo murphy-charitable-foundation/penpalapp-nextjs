aa18fe5fe0e7b141b4e139e5c7d77e2a07d04393
studentuser13 <fraserr01@gmail.com>
2025-09-01 17:31:36 -0400

    Updated /privacy-policy with analytics

--- app/privacy-policy/page.js
+++ app/privacy-policy/page.js
@@ -1,4 +1,7 @@
+
 // pages/privacy-policy.js
+"use client"
+
 import Link from "next/link";
 import Image from "next/image";
 import logo from "/public/murphylogo.png";
@@ -6,8 +9,24 @@ import { BackButton } from "../../components/general/BackButton";
 import { PageBackground } from "../../components/general/PageBackground";
 import { PageContainer } from "../../components/general/PageContainer";
 import { PageHeader } from "../../components/general/PageHeader";
+import { useEffect } from "react";
+import { usePageAnalytics } from "../useAnalytics";
+import { logLoadingTime } from "../utils/analytics";
 
 export default function TermsCondition() {
+  usePageAnalytics("/privacy-policy");
+
+  useEffect(() => {
+    const startTime = performance.now();
+    requestAnimationFrame(() => {
+      setTimeout(() => {
+        const endTime = performance.now();
+        const loadTime = endTime - startTime;
+        console.log(`Page render time: ${loadTime}ms`);
+        logLoadingTime("/privacy-policy", loadTime);
+      }, 0);
+    });
+  }, []);
   return (
     //<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4">
     <PageBackground>
@@ -16,7 +35,7 @@ export default function TermsCondition() {
 
         <div className="flex flex-col gap-10 mt-16">
           <div
-            class="container"
+            className="container"
             className="flex items-center justify-between  word-break:break-all"
           >
             <p className="font-medium font-inter text-black leading-[1.8rem] margin-20px">
@@ -31,7 +50,7 @@ export default function TermsCondition() {
           </div>
 
           <div
-            class="container"
+            className="container"
             className="flex items-center justify-between  word-break:break-all"
           >
             <p className="font-medium font-inter text-black leading-[1.8rem]">
