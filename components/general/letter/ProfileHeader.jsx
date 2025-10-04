e0e2eb0bc31d35297791759eeecae7cc4699f122
Suyeong <81769862+su794@users.noreply.github.com>
2025-10-04 10:59:25 -0700

    tweaks
    - remove country name below the name in the letterhome
    - add navbar except message pages
    - fixed the bug on loading spinner

--- components/general/letter/ProfileHeader.jsx
+++ components/general/letter/ProfileHeader.jsx
@@ -4,15 +4,14 @@ import Button from "../Button";
 
 export default function ProfileHeader({ userName, country, profileImage, id }) {
   return (
-    <header className="flex justify-between items-center bg-blue-100 p-5 border-b border-gray-200">
+    <header className="flex justify-between items-center bg-blue-100 px-5 border-b border-gray-200">
       <Link href={"/profile-view/" + id}>
         <Button
           btnText={
             <div className="flex items-center">
-              <ProfileImage photo_uri={profileImage} first_name={userName} />
+              <ProfileImage photo_uri={profileImage} first_name={userName} size={6} />
               <div className="ml-3">
               <div className="font-semibold text-lg">{userName}</div>
-              <div className="text-sm text-gray-600">{country}</div>
             </div>
             </div>
           }
