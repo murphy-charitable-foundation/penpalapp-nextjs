aa3461c73990dbaf6198d28ba9477f49e9f474a8
Rez <rez@murphycharity.org>
2025-12-03 23:40:42 -0500

    rephrase the rejected to not sent

--- app/letters/[id]/page.js
+++ app/letters/[id]/page.js
@@ -1003,7 +1003,7 @@ export default function Page({ params }) {
                                 <div className="flex items-start text-red-700 font-semibold">
                                   <AlertTriangle className="w-5 h-5 mr-2 mt-0.5" />
                                   <div>
-                                    <div>Your letter was rejected.</div>
+                                    <div>Your letter was not sent.</div>
 
                                     {message.rejection_reason && (
                                       <div className="text-sm text-red-600 mt-1">
