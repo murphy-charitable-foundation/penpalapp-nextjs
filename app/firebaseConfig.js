6f3bd6357b98747e18afcd04553fb9d7b474eff6
ebertc <conwebert@gmail.com>
2025-12-24 13:22:47 -0800

    debugging code for vercel

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -92,7 +92,7 @@ export const handleNotificationSetup = async () => {
     if (res.ok) {
       console.log("Notification setup complete:");
     } else {
-      console.log(res);
+      console.log(data);
       console.error("Server error setting up notifications:");
     }
   } catch (err) {
