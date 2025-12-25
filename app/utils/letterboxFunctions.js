84ff45e8b054e23c2c9a08c65f40afaa48486ce7
tinagalido <69448352+tinagalido@users.noreply.github.com>
2025-12-24 17:22:09 -0800

    Update getUserPfp and fetchRecipients
    Update getUserPfp and fetchRecipients to show the chat list

--- app/utils/letterboxFunctions.js
+++ app/utils/letterboxFunctions.js
@@ -14,9 +14,20 @@ const getUserDoc = async () => {
 
 export const getUserPfp = async(uid) => {
   const path = `profile/${uid}/profile-image`;
-  const photoRef = storageRef(storage, path);
-  const downloaded = await getDownloadURL(photoRef)
-  return downloaded;
+  try {
+    const photoRef = storageRef(storage, path);
+    const downloaded = await getDownloadURL(photoRef)
+    return downloaded;
+  } catch (error) {
+    // Return null if there is no profile, so it can be checked in fetchRecipient
+    if (error.code === 'storage/object-not-found') {
+      return null;
+    }
+    console.error(error);
+    // Returns null for all other errors so it only has one fallback mechanism
+    return null;
+  }
+  
 }
 
 export const fetchLetterboxes = async () => {
@@ -254,14 +265,25 @@ export const fetchRecipients = async (id) => {
   for (const user of users) {
     const selectedUserDocRef = doc(db, "users", user.id);
     const selUser = await getDoc(selectedUserDocRef);
+    const userData = selUser.data();    // utility/helper variable
     try {
       const downloaded = await getUserPfp(user.id);
-      members.push({ ...selUser.data(), id: user.id, pfp: downloaded });
+
+      // Check here if downloaded is null and provide alternate
+      let pfpToUse;
+      if (downloaded === null) {
+        pfpToUse = userData.photo_uri;
+      } else {
+        pfpToUse = downloaded;
+      }
+      //members.push({ ...selUser.data(), id: user.id, pfp: downloaded });
+      members.push({ ...userData, id: user.id, pfp: pfpToUse });
     } catch (e) {
       logError(e, {
         description: "Error fetching user:",
       });
-      members.push({ ...selUser.data(), id: selectedUserDocRef.id, pfp: selUser.photo_uri });
+      //members.push({ ...selUser.data(), id: selectedUserDocRef.id, pfp: selUser.photo_uri });
+      members.push({ ...userData, id: selectedUserDocRef.id, pfp: userData.photo_uri });
     }
   }
   return members;
