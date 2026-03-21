b0357c785de4f34bda7d16c51b950bf1d194f3d4
Gtavarez <giselltavarez@gmail.com>
2026-03-21 01:20:53 -0400

    Fix API security responses and make createConnection atomic

--- app/api/deadchat/route.js
+++ app/api/deadchat/route.js
@@ -184,9 +184,6 @@ export async function POST(request) {
       description: "Failed to send email.",
     });
 
-    return NextResponse.json(
-      { message: "Failed to send email." },
-      { status: 500 }
-    );
+    return NextResponse.json({ message: "Failed to send email." }, { status: 500 });
   }
 }
