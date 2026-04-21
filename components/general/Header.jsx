32f060d435f75bc7ed0e958f96d55a9aa0326a6f
ConnorWhite9 <connorlwhite@icloud.com>
2026-04-21 09:15:09 -0400

    passing isloading into header.

--- components/general/Header.jsx
+++ components/general/Header.jsx
@@ -27,7 +27,11 @@ export default function Header({ activeFilter, setActiveFilter, title, status="s
       />
       <h1 className="text-2xl font-semibold">Admin user</h1>
       <div className="ml-auto">
-        <FilterButton activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
+        <FilterButton
+          activeFilter={activeFilter}
+          setActiveFilter={setActiveFilter}
+          isLoadingMore={isLoadingMore}
+        />
       </div>
     </div>
   );
