caa440401b8621adc6f25db493cbed43c3396c42
eslteacher902010 <pjkniaz@gmail.com>
2026-04-21 21:52:10 -0400

    Update components/tooltip/FirstTimeChatGuide.jsx
    Co-authored-by: rez.g <31979020+madarez@users.noreply.github.com>

--- components/tooltip/FirstTimeChatGuide.jsx
+++ components/tooltip/FirstTimeChatGuide.jsx
@@ -212,7 +212,6 @@ export default function FirstTimeChatGuide({
   };
 
   const handleUseTemplate = () => {
-    console.log("saasada");
     if (onUseTemplate) {
       onUseTemplate(defaultTemplate);
     }
