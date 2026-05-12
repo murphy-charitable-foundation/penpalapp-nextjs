740130bb0c1c903b0f350c146f5bb97d5944ca9b
eslteacher902010 <pjkniaz@gmail.com>
2026-05-11 22:35:28 -0400

    Refactor layout.js to simplify provider structure

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,10 @@ import './globals.css'
 import NavigationStateManager from '../components/loading/NavigationStateManager'
 import { Suspense } from 'react'
 import LoadingSpinner from '../components/loading/LoadingSpinner'
-import { NotificationHandler } from '../components/NotificationHandler'
+import NotificationHandler from '../components/NotificationHandler'
 import { UserProvider } from '../contexts/UserContext'
 import { NavigationProvider } from '../contexts/NavigationContext'
 import { CachedUserLoginsProvider } from './contexts/CachedUserLoginContext'
-import { InactivityProvider } from './contexts/InactivityContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,19 +24,18 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="manifest" href="/manifest.json" />
       </head>
+
       <body className={inter.className}>
         <CachedUserLoginsProvider>
           <UserProvider>
-            <InactivityProvider>
-              <NotificationHandler>
-                <NavigationProvider>
-                  <Suspense fallback={<LoadingSpinner />}>
-                    <NavigationStateManager />
-                    {children}
-                  </Suspense>
-                </NavigationProvider>
-              </NotificationHandler>
-            </InactivityProvider>
+            <NotificationHandler>
+              <NavigationProvider>
+                <Suspense fallback={<LoadingSpinner />}>
+                  <NavigationStateManager />
+                  {children}
+                </Suspense>
+              </NavigationProvider>
+            </NotificationHandler>
           </UserProvider>
         </CachedUserLoginsProvider>
       </body>
