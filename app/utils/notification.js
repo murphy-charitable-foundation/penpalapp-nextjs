fb6fa046f0010cb641d8442196d8ca774fcd2c42
Rez <rez@murphycharity.org>
2026-08-30 17:18:12 -0400

    Revert "dummy test notification"
    This reverts commit 53b43396756425034ae6dc5d6212af031f745a89.

--- app/utils/notification.js
+++ app/utils/notification.js
@@ -1,13 +1,11 @@
 import { handleNotificationSetup } from '../firebaseConfig';
 
 export const initializeNotifications = async () => {
-  console.log('[Notifications] Initializing notification flow.');
   if ('serviceWorker' in navigator) {
     try {
       await handleNotificationSetup();
-      console.log('[Notifications] Notification flow finished.');
     } catch (err) {
-      console.error('[Notifications] Notification flow failed:', err);
+      console.error('Service Worker registration failed: ', err);
     }
   } else {
     console.error('Service Workers are not supported by this browser.');
