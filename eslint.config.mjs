c365b18565b38299d3eda1cc9830205a32b711dc
paul_kniaz <pjkniaz@gmail.com>
2026-09-02 19:25:15 -0400

    chore: upgrade Next.js to 15

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  {
+    ignores: [".next/**", "node_modules/**", "out/**", "build/**"],
+  },
+  ...compat.extends("next/core-web-vitals"),
+];
+
+export default eslintConfig;
